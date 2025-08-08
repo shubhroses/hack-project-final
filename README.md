1c08dddf2bc1fc1f522d39787205cbabcf5f5dc9
shubhroses <shubhroses@gmail.com>
2025-08-08 10:15:11 -0700

    Added commands to readme used to install vagrant Wrote requirements.txt for flask app

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Step 1. Install Vagrant and QEMU provider for ARM compatibility
 
+```bash
+brew install vagrant
+brew install qemu
+
+sudo apt-get update -y
+sudo apt-get install git curl bash-completion
+curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker vagrant
+```
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eou pipefail
+
+PROJECT_DIR="/home/vagrant/hack-project-final"
+GIT_URL="https://github.com/shubhroses/hack-project-final.git"
+
+echo ""
+echo "1. Ensure Vagrant is up and running"
+vagrant up --provider=qemu
+
+echo ""
+echo "2. Git clone or git pull depending on if directory exists"
+vagrant ssh -c "
+if [ -d ${PROJECT_DIR} ]; then
+    echo 'Directory exists'
+    cd ${GIT_URL} && git pull origin main
+else
+    echo 'Directory does not exist'
+    git clone ${GIT_URL} ${PROJECT_DIR}
+fi
+"
+
