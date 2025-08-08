914e908df16cf503794109832117aff983f1bb09
shubhroses <shubhroses@gmail.com>
2025-08-08 11:12:05 -0700

    Added system-info end point that should return contents of /etc/os-release

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
+import logging
+import time
+from datetime import datetime
 from flask import Flask
+from pathlib import Path
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+file_path = "/etc/os-release"
+# file_path = "/Users/interview.candidate/codebase/hack-project-final/requirements.txt"
+
 @app.route("/")
+def hellp():
+    logging.info("Reached default endpoint")
+    return "Hello from Flask in kubernetes inside of vagrant."
+
+@app.route("/ping")
 def ping():
+    logging.info("Reached ping endpoint")
     return "pong"
 
+@app.route("/system-info")
+def system_info():
+    logging.info("Reached system-info endpoint")
+    contents = Path(file_path).read_text()
+    logging.info(f"Contents of {file_path}: {contents}")
+    return contents
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    logging.info("Starting main application...")
+    app.run(host="0.0.0.0", port=5001, debug=False)
